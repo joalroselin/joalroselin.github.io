2394f4aab2a48494fbc080523d29bbddb5e8d2b8
Joal Rose Lin <105606368+joalroselin@users.noreply.github.com>
2024-11-05 16:59:42 +0800

    added test

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/styles/index.css'
-import App from './app/App.tsx'
+import App from './app/App';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
     <App />
+    <h1>test</h1>
   // </StrictMode>,
 )
