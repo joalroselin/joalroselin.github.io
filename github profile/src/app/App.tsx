599aabe2001a9623f0fd19be3d31fabbab27e8b6
Joal Rose Lin <105606368+joalroselin@users.noreply.github.com>
2024-11-05 16:37:41 +0800

    restructure

--- github profile/src/app/App.tsx
+++ github profile/src/app/App.tsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+    <div>
+      <h1>Test app</h1>
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
